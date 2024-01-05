80f34fc08b05ac72d368c290d5a29ccdaf65cfcd
Erik Lillehagen <eblillehagen@gmail.com>
2024-01-05 15:02:41 +0000

    Updates

---
+++ _app/immutable/chunks/paths.cnabuZwk.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1ozad1w?.base??"/sveltekit-github-pages",a=globalThis.__sveltekit_1ozad1w?.assets??s;export{a,s as b};
--- _app/immutable/chunks/paths.faq5vR7r.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_8i3s8h?.base??"/sveltekit-github-pages",e=globalThis.__sveltekit_8i3s8h?.assets??s;export{e as a,s as b};
